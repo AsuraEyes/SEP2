bf7683669030e1b1453521eb8e15bc2ab18b4249
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-05-13 15:05:12 +0200

    commit

---
+++ Database/dummy_data.sql
@@ -0,0 +1,5 @@
+SET SCHEMA 'share_it';
+
+INSERT INTO city (name)
+VALUES('CopenHagen'), ('Aarhus'), ('Vejle'), ('Odense'), ('Aalborg'),
+       ('Esbjerg'), ('Randers'), ('Kolding'), ('Horsens'), ('Roskilde');
