b591a2befc83fb82b55545d4d94e6b60a1e8964a
Maggie <maki.lapcik@gmail.com>
2021-05-13 15:10:07 +0100

    d

--- Database/share_it.sql
+++ Database/share_it.sql
@@ -14,3 +14,35 @@ CREATE TABLE category(
 CREATE TABLE state(
     name VARCHAR (100) PRIMARY KEY
 );
+
+CREATE TABLE member(
+    username VARCHAR (100) PRIMARY KEY,
+    password VARCHAR (100),
+    email_address VARCHAR (200),
+    other_information bla_bla_type,
+    address_street VARCHAR (100),
+    address_no VARCHAR(100),
+    address_postal_code int,
+    address_city_name VARCHAR (100),
+    FOREIGN KEY (address_city_name) REFERENCES city(name)
+);
+
+CREATE TABLE rating(
+    value int CHECK ( value > 0 AND value < 6 ),
+    commentary bla_bla_type,
+    member_from VARCHAR (100),
+    member_to VARCHAR (100),
+    PRIMARY KEY (member_from, member_to),
+    FOREIGN KEY (member_from) REFERENCES member(username),
+    FOREIGN KEY (member_to) REFERENCES member(username)
+);
+
+CREATE TABLE message(
+    text bla_bla_type,
+    time TIMESTAMP,
+    member_from VARCHAR (100),
+    member_to VARCHAR (100),
+    PRIMARY KEY (member_from, member_to),
+    FOREIGN KEY (member_from) REFERENCES member(username),
+    FOREIGN KEY (member_to) REFERENCES member(username)
+);
