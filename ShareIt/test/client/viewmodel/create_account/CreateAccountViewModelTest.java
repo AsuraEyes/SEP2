cc662abc3d2811820f854a392a26d8ee5cdb9434
143473 <75113646+143473@users.noreply.github.com>
2021-05-17 17:45:25 +0200

    a

--- ShareIt/test/client/viewmodel/create_account/CreateAccountViewModelTest.java
+++ ShareIt/test/client/viewmodel/create_account/CreateAccountViewModelTest.java
@@ -1,4 +1,4 @@
-package client.viewmodel.create_account;
+/*package client.viewmodel.create_account;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -38,4 +38,4 @@ class CreateAccountViewModelTest {
 //    }
 
 
-}
+}*/
