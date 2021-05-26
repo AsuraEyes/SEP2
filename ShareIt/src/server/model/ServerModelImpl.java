a9f71f37bbb1310d0fe0f7e42611cfe96accbedc
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-05-26 08:53:54 +0200

    receive warning

--- ShareIt/src/server/model/ServerModelImpl.java
+++ ShareIt/src/server/model/ServerModelImpl.java
@@ -14,7 +14,6 @@ import shared.transferobjects.*;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +22,6 @@ public class ServerModelImpl implements ServerModelManager
 {
   private PropertyChangeSupport support;
   private DataCheckMember dataCheckMember;
-  private DataCheckMember updateDataCheckMember;
   private DataCheckRental dataCheckRental;
   private DataCheckSearch dataCheckSearch;
   private DataCheckRating dataCheckRating;
@@ -38,18 +36,14 @@ public class ServerModelImpl implements ServerModelManager
     dataCheckReport = new DataCheckReport();
   }
 
-  @Override public void addListener(String propertyName,
-      PropertyChangeListener listener)
-  {
+  @Override public void addListener(String propertyName, PropertyChangeListener listener) {
     if(propertyName != null)
       support.addPropertyChangeListener(propertyName, listener);
     else
       support.addPropertyChangeListener(listener);
   }
 
-  @Override public void removeListener(String propertyName,
-      PropertyChangeListener listener)
-  {
+  @Override public void removeListener(String propertyName, PropertyChangeListener listener) {
     if(propertyName != null)
       support.removePropertyChangeListener(propertyName, listener);
     else
@@ -58,16 +52,12 @@ public class ServerModelImpl implements ServerModelManager
 
   @Override
   public String checkMemberData(String username, String password, String confirmPassword, String email, String otherInformation, String phone, String street, String streetNo, String postalCode, String city) {
-    String message = dataCheckMember.checkData(username, password, confirmPassword, email, otherInformation, phone, street, streetNo, postalCode, city);
-
-    //support.firePropertyChange("dataValidation", 0, message);
-    return message;
+    return dataCheckMember.checkData(username, password, confirmPassword, email, otherInformation, phone, street, streetNo, postalCode, city);
   }
 
   @Override
-  public String updateCheckMemberData(String username, String password, String confirmPassword, String email, String phone, String otherInformation, String street, String streetNo, String postalCode, String city) throws IOException {
-    String message = dataCheckMember.updateCheckData(username, password, confirmPassword, email, otherInformation, phone, street, streetNo, postalCode, city);
-    return message;
+  public String updateCheckMemberData(String username, String password, String confirmPassword, String email, String phone, String otherInformation, String street, String streetNo, String postalCode, String city) {
+    return dataCheckMember.updateCheckData(username, password, confirmPassword, email, otherInformation, phone, street, streetNo, postalCode, city);
   }
 
   @Override
@@ -86,24 +76,16 @@ public class ServerModelImpl implements ServerModelManager
   }
 
   @Override
-  public List<Rental> checkSearch(String search)
-  {
-    List<Rental> list = dataCheckSearch.checkSearch(search);
-    return list;
+  public List<Rental> checkSearch(String search) {
+    return dataCheckSearch.checkSearch(search);
   }
 
-  @Override public String addFeedback(double starValue, String feedback, String username1, String username2)
-  {
-
-    String message = dataCheckRating.addFeedback(starValue, feedback, username1, username2);
-    return message;
+  @Override public String addFeedback(double starValue, String feedback, String username1, String username2) {
+    return dataCheckRating.addFeedback(starValue, feedback, username1, username2);
   }
 
-  @Override public String addReport(String feedback, String username1,
-      String username2)
-  {
-    String message = dataCheckReport.addReport(feedback,username1,username2);
-    return message;
+  @Override public String addReport(String feedback, String username1, String username2) {
+    return dataCheckReport.addReport(feedback,username1,username2);
   }
 
   @Override
@@ -139,28 +121,22 @@ public class ServerModelImpl implements ServerModelManager
     return null;
   }
 
-  @Override public ArrayList<Rental> getRentalsList()
-  {
-    try
-    {
+  @Override public ArrayList<Rental> getRentalsList() {
+    try {
       return (ArrayList<Rental>) RentalDAOImpl.getInstance().readRentals();
     }
-    catch (SQLException e)
-    {
+    catch (SQLException e) {
       e.printStackTrace();
     }
     return null;
   }
 
-  @Override public Member getMemberById(int id)
-  {
-    try
-    {
+  @Override public Member getMemberById(int id) {
+    try {
       return MemberDAOImpl.getInstance().getMemberById(id);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return null;
   }
@@ -220,65 +196,50 @@ public class ServerModelImpl implements ServerModelManager
     return false;
   }
 
-  @Override public Rating getRating(String fromUsername, String toUsername)
-  {
-    try
-    {
+  @Override public Rating getRating(String fromUsername, String toUsername) {
+    try {
       return RatingDAOImpl.getInstance().getRating(fromUsername, toUsername);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return null;
   }
 
-  @Override public Report getReport(String fromUsername, String toUsername)
-  {
-    try
-    {
+  @Override public Report getReport(String fromUsername, String toUsername) {
+    try {
       return ReportDAOImpl.getInstance().getReport(fromUsername, toUsername);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return null;
   }
 
-  @Override public void updateRating(Rating rating)
-  {
-    try
-    {
+  @Override public void updateRating(Rating rating) {
+    try {
       RatingDAOImpl.getInstance().updateRating(rating);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
   }
 
-  @Override public void updateReport(Report report)
-  {
-    try
-    {
+  @Override public void updateReport(Report report) {
+    try {
       ReportDAOImpl.getInstance().updateReport(report);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
   }
 
-  @Override public boolean deleteRental(Rental rental)
-  {
-    try
-    {
+  @Override public boolean deleteRental(Rental rental) {
+    try {
       return RentalDAOImpl.getInstance().delete(rental);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return false;
   }
@@ -288,8 +249,8 @@ public class ServerModelImpl implements ServerModelManager
     try{
       return MemberDAOImpl.getInstance().readByUsername(value);
     }
-    catch (SQLException throwables){
-      throwables.printStackTrace();
+    catch (SQLException e){
+      e.printStackTrace();
     }
     return null;
   }
@@ -305,8 +266,7 @@ public class ServerModelImpl implements ServerModelManager
     return null;
   }
 
-  @Override public List<Report> getReportList()
-  {
+  @Override public List<Report> getReportList() {
     try{
       return ReportDAOImpl.getInstance().readReports();
     }
@@ -316,60 +276,47 @@ public class ServerModelImpl implements ServerModelManager
     return null;
   }
 
-
-
-  @Override public ArrayList<Message> getAllReceivedMessages(int loggedUserId)
-  {
-    try
-    {
+  @Override public ArrayList<Message> getAllReceivedMessages(int loggedUserId) {
+    try {
       return MessageDAOImpl.getInstance().getAllReceivedMessages(loggedUserId);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return null;
   }
 
-  @Override public ArrayList<Message> getMessagesFromUser(int loggedUserId,
-      int fromUserid)
-  {
-    try
-    {
+  @Override public ArrayList<Message> getMessagesFromUser(int loggedUserId, int fromUserid) {
+    try {
       return MessageDAOImpl
           .getInstance().getMessagesFromUser(loggedUserId, fromUserid);
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
     return null;
   }
 
   @Override
   public ArrayList<Warning> getWarnings(String administrator, int idTo) {
-    try
-    {
+    try {
       return WarningDAOImpl.getInstance().getWarnings(administrator, idTo);
     }
-    catch (SQLException e)
-    {
+    catch (SQLException e) {
       e.printStackTrace();
     }
     return null;
   }
 
-  @Override public void sendMessage(Message message)
-  {
-    try
-    {
+  @Override public void sendMessage(Message message) {
+    try {
       support.firePropertyChange("newMessage", 0, MessageDAOImpl.getInstance().sendMessage(message));
     }
-    catch (SQLException throwables)
-    {
-      throwables.printStackTrace();
+    catch (SQLException e) {
+      e.printStackTrace();
     }
   }
+
   @Override public void sendWarning(Warning warning){
     try {
       support.firePropertyChange("newWarning", 0, WarningDAOImpl.getInstance().sendWarning(warning));
