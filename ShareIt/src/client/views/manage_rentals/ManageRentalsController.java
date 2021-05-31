7ab25b80077f9b04e032eef206a9b3fac9771b9a
AsuraEyes <56295945+AsuraEyes@users.noreply.github.com>
2021-05-31 10:51:51 +0200

    css for chat added

--- ShareIt/src/client/views/manage_rentals/ManageRentalsController.java
+++ ShareIt/src/client/views/manage_rentals/ManageRentalsController.java
@@ -41,7 +41,7 @@ public class ManageRentalsController {
     categoryLabel.textProperty().bind(manageRentalsViewModel.categoryOfRentalProperty());
   }
 
-  public void changeButton(){
+  public void editButton(){
     viewHandler.setView(viewHandler.menu(), viewHandler.editRental());
     manageRentalsViewModel.getSelectedRental();
   }
