d18a20e48247cac0bbcfba56fedaf38ac3380928
143473 <75113646+143473@users.noreply.github.com>
2021-06-02 11:09:45 +0200

    b

--- ShareIt/src/client/model/member/MemberModel.java
+++ ShareIt/src/client/model/member/MemberModel.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface MemberModel extends Subject
 {
   /**
-   * Gets data of new member from ViewModel and sends it to the Client.
+   * Middle point between Client and ViewModels handling member data
    *
    * @param username         the username
    * @param password         the password
@@ -24,7 +24,7 @@ public interface MemberModel extends Subject
    * @param streetNo         the street no
    * @param postalCode       the postal code
    * @param city             the city
-   * @return returns data from Client
+   * @return data from Client
    */
   String checkMemberData(String username, String password,
       String confirmPassword, String email, String phone,
@@ -43,7 +43,7 @@ public interface MemberModel extends Subject
    * @param streetNo         the street no
    * @param postalCode       the postal code
    * @param city             the city
-   * @return returns data from Client
+   * @return data from Client
    */
   String updateCheckMemberData(String username, String password,
       String confirmPassword, String email, String phone,
@@ -52,7 +52,7 @@ public interface MemberModel extends Subject
   /**
    * Checks user type by getting data from ViewModel.
    *
-   * @return returns String type of user that is using an application
+   * @return String type of user that is using an application
    */
   String checkUserType();
   /**
@@ -60,43 +60,44 @@ public interface MemberModel extends Subject
    *
    * @param username the username
    * @param password the password
-   * @return returns data from Client
+   * @return data from Client
    */
   String checkLogInCredentials(String username, String password);
   /**
-   * Gets logged in username from ViewModel.
+   * Gets logged in username from the StateManager.
    *
-   * @return returns logged in username
+   * @return logged in username
    */
   String getLoggedInUsername();
+
   /**
-   * Gets member by id with data from ViewModel and send it to the Client.
+   * Gets member by id with data from the database.
    *
    * @param id the id
-   * @return returns Member's ID from Client
+   * @return Member's ID from Client
    */
   Member getMemberById(int id);
   /**
-   * Gets member username with data from ViewModel.
+   * Gets member username from MemberModelManager.
    *
-   * @return returns Member's username
+   * @return Member's username
    */
   String getMemberUsername();
   /**
-   * Sets member username with data from ViewModel.
+   * Sets member username from MemberModelManager.
    *
    * @param memberUsername Member's username
    */
   void setMemberUsername(String memberUsername);
   /**
-   * Gets member by username with data from ViewModel and sends it to the Client.
+   * Gets member by username with data from database.
    *
    * @param memberUsername Member's username
-   * @return returns Member object matched with username from Client
+   * @return Member object matched with username from Client
    */
   Member getMemberByUsername(String memberUsername);
   /**
-   * Gets members list from Client.
+   * Gets members list from database.
    *
    * @return the members list
    */
@@ -118,35 +119,35 @@ public interface MemberModel extends Subject
   List<Member> checkSearchForMember(String value);
 
   /**
-   * Sets selected report.
+   * Sets selected report in the MemberModelManager.
    *
    * @param reporterNameLabel Member that reports
    * @param reportedNameLabel Member that is being reported
    */
   void setSelectedReport(String reporterNameLabel, String reportedNameLabel);
   /**
-   * Gets report list from Client.
+   * Gets report list from database.
    *
-   * @return returns a list of all the reports
+   * @return a list of all the reports
    */
   List<Report> getReportList();
   /**
-   * Gets selected report.
+   * Gets selected report from MemberModelManager.
    *
-   * @return returns selected report
+   * @return selected report
    */
   Report getSelectedReport();
 
   /**
-   * Checks members data before deleting by sending it to the Client.
+   * Checks members data before deleting by sending it to the Client and deletes it.
    *
    * @param member Member that is being deleted
-   * @return returns true if data was validated properly
+   * @return true if data was validated properly
    */
   boolean deleteMember(Member member);
 
   /**
-   * Sets report list.
+   * Sets report list in the MemberModelManager.
    */
   void setReportList();
 }
